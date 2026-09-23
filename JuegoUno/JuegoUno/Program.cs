377871c548776774d38685652f165f3b02b31f4f
aleeareevalo <alearevalo.w@gmail.com>
2026-09-22 18:35:40 -0600

    Ignorar archivos

--- JuegoUno/JuegoUno/Program.cs
+++ JuegoUno/JuegoUno/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MySqlConnector;
+
+string connectionString = File.ReadAllText("config.txt").Trim();
+
+using var connection = new MySqlConnection(connectionString);
+
+try
+{
+    connection.Open();
+    Console.WriteLine("¡Conexión exitosa a juego_uno! 🎉");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error al conectar: " + ex.Message);
+}
